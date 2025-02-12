8c27c6ba2e74a65a259527945fe3a1aa8f14a3c0
Vinicius Flores <viniciusflores379@gmail.com>
2025-02-12 19:34:15 -0300

    first commit

---
+++ tests/test_dependencies.py
@@ -0,0 +1,12 @@
+from unittest import TestCase
+
+
+class TestMain(TestCase):
+    def test_dependencies(self):
+        self.assertTrue(True)
+
+    def test_dependencies2(self):
+        self.assertTrue(True)
+
+    def test_dependencies3(self):
+        self.assertTrue(True)
---
+++ tests/test_main.py
@@ -0,0 +1,15 @@
+from unittest import TestCase
+
+
+class TestMain(TestCase):
+    def test_main(self):
+        self.assertTrue(True)
+
+    def test_main2(self):
+        self.assertTrue(True)
+
+    def test_main3(self):
+        self.assertTrue(True)
+
+    # def test_main4(self):
+    #     self.assertTrue(False)
