c45b42f25b6a5b4f43d541b4640de44e69a6a9c4
Vinicius Flores <viniciusflores379@gmail.com>
2025-03-28 18:59:44 -0300

    test p02

---
+++ tests/test_a.py
@@ -0,0 +1,329 @@
+import pytest
+import time
+
+## Initializing 10 tests with 1s intentional delay
+
+
+def test_with_xdist_01():
+    time.sleep(1)
+    assert True
+
+
+def test_with_xdist_02():
+    time.sleep(1)
+    assert True
+
+
+def test_with_xdist_03():
+    time.sleep(1)
+    assert True
+
+
+def test_with_xdist_04():
+    time.sleep(1)
+    assert True
+
+
+def test_with_xdist_05():
+    time.sleep(1)
+    assert True
+
+
+def test_with_xdist_06():
+    time.sleep(1)
+    assert True
+
+
+def test_with_xdist_07():
+    time.sleep(1)
+    assert True
+
+
+def test_with_xdist_08():
+    time.sleep(1)
+    assert True
+
+
+def test_with_xdist_09():
+    time.sleep(1)
+    assert True
+
+
+def test_with_xdist_10():
+    time.sleep(1)
+    assert True
+
+
+##################################################################
+
+## Initializing 10 tests with 1s intentional delay
+
+
+def test_with_xdist_01a():
+    time.sleep(1)
+    assert True
+
+
+def test_with_xdist_02a():
+    time.sleep(1)
+    assert True
+
+
+def test_with_xdist_03a():
+    time.sleep(1)
+    assert True
+
+
+def test_with_xdist_04a():
+    time.sleep(1)
+    assert True
+
+
+def test_with_xdist_05a():
+    time.sleep(1)
+    assert True
+
+
+def test_with_xdist_06a():
+    time.sleep(1)
+    assert True
+
+
+def test_with_xdist_07a():
+    time.sleep(1)
+    assert True
+
+
+def test_with_xdist_08a():
+    time.sleep(1)
+    assert True
+
+
+def test_with_xdist_09a():
+    time.sleep(1)
+    assert True
+
+
+def test_with_xdist_10a():
+    time.sleep(1)
+    assert True
+
+
+##################################################################
+## Initializing 10 tests with 1s intentional delay
+
+
+def test_with_xdist_01b():
+    time.sleep(1)
+    assert True
+
+
+def test_with_xdist_02b():
+    time.sleep(1)
+    assert True
+
+
+def test_with_xdist_03b():
+    time.sleep(1)
+    assert True
+
+
+def test_with_xdist_04b():
+    time.sleep(1)
+    assert True
+
+
+def test_with_xdist_05b():
+    time.sleep(1)
+    assert True
+
+
+def test_with_xdist_06b():
+    time.sleep(1)
+    assert True
+
+
+def test_with_xdist_07b():
+    time.sleep(1)
+    assert True
+
+
+def test_with_xdist_08b():
+    time.sleep(1)
+    assert True
+
+
+def test_with_xdist_09b():
+    time.sleep(1)
+    assert True
+
+
+def test_with_xdist_10b():
+    time.sleep(1)
+    assert True
+
+
+##################################################################
+## Initializing 10 tests with 1s intentional delay
+
+
+def test_with_xdist_01c():
+    time.sleep(1)
+    assert True
+
+
+def test_with_xdist_02c():
+    time.sleep(1)
+    assert True
+
+
+def test_with_xdist_03c():
+    time.sleep(1)
+    assert True
+
+
+def test_with_xdist_04c():
+    time.sleep(1)
+    assert True
+
+
+def test_with_xdist_05c():
+    time.sleep(1)
+    assert True
+
+
+def test_with_xdist_06c():
+    time.sleep(1)
+    assert True
+
+
+def test_with_xdist_07c():
+    time.sleep(1)
+    assert True
+
+
+def test_with_xdist_08c():
+    time.sleep(1)
+    assert True
+
+
+def test_with_xdist_09c():
+    time.sleep(1)
+    assert True
+
+
+def test_with_xdist_10c():
+    time.sleep(1)
+    assert True
+
+
+##################################################################
+## Initializing 10 tests with 1s intentional delay
+
+
+def test_with_xdist_01d():
+    time.sleep(1)
+    assert True
+
+
+def test_with_xdist_02d():
+    time.sleep(1)
+    assert True
+
+
+def test_with_xdist_03d():
+    time.sleep(1)
+    assert True
+
+
+def test_with_xdist_04d():
+    time.sleep(1)
+    assert True
+
+
+def test_with_xdist_05d():
+    time.sleep(1)
+    assert True
+
+
+def test_with_xdist_06d():
+    time.sleep(1)
+    assert True
+
+
+def test_with_xdist_07d():
+    time.sleep(1)
+    assert True
+
+
+def test_with_xdist_08d():
+    time.sleep(1)
+    assert True
+
+
+def test_with_xdist_09d():
+    time.sleep(1)
+    assert True
+
+
+def test_with_xdist_10d():
+    time.sleep(1)
+    assert True
+
+
+##################################################################
+
+## Initializing 10 tests with 1s intentional delay
+
+
+def test_with_xdist_01e():
+    time.sleep(1)
+    assert True
+
+
+def test_with_xdist_02e():
+    time.sleep(1)
+    assert True
+
+
+def test_with_xdist_03e():
+    time.sleep(1)
+    assert True
+
+
+def test_with_xdist_04e():
+    time.sleep(1)
+    assert True
+
+
+def test_with_xdist_05e():
+    time.sleep(1)
+    assert True
+
+
+def test_with_xdist_06e():
+    time.sleep(1)
+    assert True
+
+
+def test_with_xdist_07e():
+    time.sleep(1)
+    assert True
+
+
+def test_with_xdist_08e():
+    time.sleep(1)
+    assert True
+
+
+def test_with_xdist_09e():
+    time.sleep(1)
+    assert True
+
+
+def test_with_xdist_10e():
+    time.sleep(1)
+    assert True
+
+
+##################################################################
