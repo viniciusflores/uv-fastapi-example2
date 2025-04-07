f49ce286c4361450e8d3a8059915ddbae1311f1a
Vinicius Flores <viniciusflores379@gmail.com>
2025-04-07 09:31:30 -0300

    remove test force fail

--- tests/test_main.py
+++ tests/test_main.py
@@ -13,8 +13,8 @@ class TestMain(TestCase):
     # def test_main3(self):
     #     self.assertTrue(True)
 
-    def test_main4(self):
-        self.assertTrue(False)
+    # def test_main4(self):
+    #     self.assertTrue(False)
 
     # def test_main5(self):
     #     assert 1 == 2
